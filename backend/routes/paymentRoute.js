c652c82506f742e10fcd2ed4bf81a49330d5ad0a
Prakhar Pande <pandeprakhar1801@gmail.com>
2025-06-03 13:29:26 +0530

    order amount added

--- backend/routes/paymentRoute.js
+++ backend/routes/paymentRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const paymentController = require("../controllers/paymentController");
 
-router.get("/api/cashfree/session/:userId/:userName/:userEmail", paymentController.getSessionId);
+router.get("/api/cashfree/session/:userId/:userName/:userEmail/:total", paymentController.getSessionId);
 router.get("/api/cashfree/verify/:orderId", paymentController.verifyPayment);
 
 module.exports = router;
