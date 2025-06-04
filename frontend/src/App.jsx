9b0974328ca49b5e2e200cdee5bf78b9ba57d939
Prakhar Pande <pandeprakhar1801@gmail.com>
2025-06-04 23:15:33 +0530

    app routes changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,6 +39,7 @@ function App() {
           <Route path="/orders" element={<ProtectedRoute element={<Orders />} />} />
           <Route path="/profile" element={<ProtectedRoute element={<Profile />} />} />
           <Route path="/checkout" element={<ProtectedRoute element={<Checkout />} />} />
+          <Route path="/check" element={<PaymentStatus />} />
           <Route path="/payment-status" element={<ProtectedRoute element={<PaymentStatus />} />} />
           <Route path="/search" element={<ProtectedRoute element={<SearchResults />} />} />
 
